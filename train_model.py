0e7a8e087c689d074d11498f24c444cdc139e86f
BABATOUNDE MOCTARD <54064640+BeObm@users.noreply.github.com>
2025-09-17 08:50:44 +0200

    update Training

--- train_model.py
+++ train_model.py
@@ -12,9 +12,9 @@ def main():
     # Load a YOLOv8 model (nano is fastest, switch to 'yolov8s' or 'yolov8m' for better accuracy)
     model = YOLO("yolo12s.pt")  # Pretrained base model
 
-    # ls=range(2)
+    ls=range(200)
     # Train the model
-    model.train(task='detect', data="dataset.yaml",patience=75,epochs=900, imgsz=640,batch=16,overlap_mask=True,workers=32)
+    model.train(task='detect', data="dataset.yaml",freeze=ls,patience=75,epochs=980, imgsz=640,batch=8,overlap_mask=False,workers=32)
 
     # Save final weights
     model.export(format="onnx")  # Optional export
