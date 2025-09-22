19ed492670b2c6790c32fe921f05f341adf74773
BABATOUNDE MOCTARD <54064640+BeObm@users.noreply.github.com>
2025-09-22 11:05:32 +0200

    update

--- train_model.py
+++ train_model.py
@@ -1,7 +1,7 @@
 from onnxslim.core import freeze
 # from torch.optim.adamw import AdamW
 from ultralytics import YOLO
-from win32comext.shell.demos.servers.folder_view import tasks
+# from win32comext.shell.demos.servers.folder_view import tasks
 
 from utils import setup_dataset
 
@@ -10,12 +10,12 @@ def main():
     setup_dataset()
 
     # Load a YOLOv8 model (nano is fastest, switch to 'yolov8s' or 'yolov8m' for better accuracy)
-    model = YOLO("yolo12s.pt")  # Pretrained base model
+    model = YOLO("yolo12s.pt").load("best.pt")  # Pretrained base model
 
     # ls=range(200)
     # Train the model
-    model.train(task='detect', data="dataset.yaml",device=2, patience=75, epochs=5000, imgsz=640, batch=16, single_cls=True,
-                overlap_mask=False, box=11.0, plots=True, workers=32)
+    model.train(task='detect', data="dataset.yaml",device=[0,1,2,3], patience=75, epochs=850, imgsz=640, batch=32, single_cls=True,
+                overlap_mask=False, box=11.0, workers=32,optimizer="SGD", cos_lr=True)
     # Save final weights
     model.export(format="onnx")  # Optional export
     print("Training completed!")
