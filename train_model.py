31114a135b82556dc85a1dfa93898aec4faf693f
BABATOUNDE MOCTARD <54064640+BeObm@users.noreply.github.com>
2025-10-16 09:48:34 +0200

    update

--- train_model.py
+++ train_model.py
@@ -1,4 +1,5 @@
 from onnxslim.core import freeze
+from statsmodels.graphics.mosaicplot import mosaic
 # from torch.optim.adamw import AdamW
 from ultralytics import YOLO,NAS
 # from win32comext.shell.demos.servers.folder_view import tasks
@@ -10,12 +11,12 @@ def main():
     setup_dataset()
 
     # Load a YOLOv8 model (nano is fastest, switch to 'yolov8s' or 'yolov8m' for better accuracy)
-    model = YOLO("yolo11m.pt")  # Pretrained base model
+    model = YOLO("yolo11m.pt").load("best.pt")  # Pretrained base model
 
-    ls=range(200)
+    # ls=range(200)
     # Train the model
-    model.train(task='detect', freeze=ls,data="dataset.yaml", patience=75, epochs=1000, imgsz=640, batch=32, single_cls=True,
-                overlap_mask=False, box=11.0, workers=32, cos_lr=True)
+    model.train(model="args.yaml", task='detect',data="dataset.yaml",patience=200, epochs=2500, imgsz=640, batch=8,overlap_mask=False, box=8.0,
+                cutmix=0.6, mixup=0.7,mosaic=1.0,bgr=0.7, fliplr=0.5, flipud=0.6, shear=0.5, translate=0.7, degrees=0.4, weight_decay=0.001, lr0=0.001,close_mosaic=100,save_period=-1)
     # Save final weights
     model.export(format="onnx")  # Optional export
     print("Training completed!")
